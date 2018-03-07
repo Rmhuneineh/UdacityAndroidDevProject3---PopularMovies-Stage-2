a2df603f983cffc79e950277b798a66dcb055e6a
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2018-03-07 18:15:02 +0100

    BindViews in MainActivity

--- app/src/main/java/com/example/android/popmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popmovies/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.android.popmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "MainActivity";
+
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.error_tv)
+    TextView mErrorTV;
+
+    @BindView(R.id.progress_bar)
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(LOG_TAG, "OnCreate Called!");
     }
 }
