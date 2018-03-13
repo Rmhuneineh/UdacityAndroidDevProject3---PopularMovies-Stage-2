83184e5ae80358befda0891a102c7552563c4bf0
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2018-03-14 00:26:42 +0100

    Set Favorites and Loader in MainActivity

--- app/src/main/java/com/example/android/popmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popmovies/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.android.popmovies;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,6 +18,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -22,9 +27,12 @@ import java.util.ArrayList;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String LOG_TAG = "MainActivity";
+
+    private static final int FAVORITES_LOADER = 0;
 
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
@@ -35,8 +43,12 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.progress_bar)
     ProgressBar mProgressBar;
 
+    @BindView(R.id.empty_view)
+    LinearLayout emptyView;
+
     ArrayList<Movie> mMoviesList = new ArrayList<Movie>();
     MovieRecyclerAdapter mRecyclerAdapter;
+    MovieCursorAdapter mMovieCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,29 +63,75 @@ public class MainActivity extends AppCompatActivity {
                 getResources().getInteger(R.integer.column_number));
         mRecyclerView.setLayoutManager(gridLayoutManager);
         mRecyclerView.setHasFixedSize(true);
-        mRecyclerAdapter = new MovieRecyclerAdapter(this, mMoviesList);
-        mRecyclerView.setAdapter(mRecyclerAdapter);
 
-        if(hasConnection()) {
-            mRecyclerView.setVisibility(View.GONE);
-            mProgressBar.setVisibility(View.VISIBLE);
-            mErrorTV.setVisibility(View.GONE);
+        SharedPreferences sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(this);
 
-            SharedPreferences sharedPreferences = PreferenceManager
-                    .getDefaultSharedPreferences(this);
+        String sortOrderKey = getString(R.string.key);
+        String sortOrderDefault = getString(R.string.default_value);
+        String sortOrder = sharedPreferences.getString(sortOrderKey, sortOrderDefault);
 
-            String sortOrderKey = getString(R.string.key);
-            String sortOrderDefault = getString(R.string.default_value);
-            String sortOrder = sharedPreferences.getString(sortOrderKey, sortOrderDefault);
+        switch (sortOrder) {
+            case "popular":
+            case "top_rated":
+                mRecyclerAdapter = new MovieRecyclerAdapter(this, mMoviesList);
+                mRecyclerView.setAdapter(mRecyclerAdapter);
 
-            FetchTask movieTask = new FetchTask(this, mRecyclerAdapter);
-            movieTask.execute(sortOrder);
-        } else {
-            mRecyclerView.setVisibility(View.GONE);
-            mProgressBar.setVisibility(View.GONE);
-            mErrorTV.setText(R.string.error_message);
-            mErrorTV.setVisibility(View.VISIBLE);
+                if(hasConnection()) {
+                    mRecyclerView.setVisibility(View.GONE);
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mErrorTV.setVisibility(View.GONE);
+
+                    FetchTask movieTask = new FetchTask(this, mRecyclerAdapter);
+                    movieTask.execute(sortOrder);
+                } else {
+                    mRecyclerView.setVisibility(View.GONE);
+                    mProgressBar.setVisibility(View.GONE);
+                    mErrorTV.setText(R.string.error_message);
+                    mErrorTV.setVisibility(View.VISIBLE);
+                }
+                break;
+
+            case "favorites":
+                mMovieCursorAdapter = new MovieCursorAdapter(this, null);
+                mRecyclerView.setAdapter(mMovieCursorAdapter);
+                getSupportLoaderManager().initLoader(FAVORITES_LOADER, null, this);
         }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] projection = {
+                FavoritesContract.FavoritesEntry._ID,
+                FavoritesContract.FavoritesEntry.COLUMN_MOVIE_ID,
+                FavoritesContract.FavoritesEntry.COLUMN_MOVIE_TITLE,
+                FavoritesContract.FavoritesEntry.COLUMN_MOVIE_DATE,
+                FavoritesContract.FavoritesEntry.COLUMN_MOVIE_POSTER,
+                FavoritesContract.FavoritesEntry.COLUMN_MOVIE_RATING,
+                FavoritesContract.FavoritesEntry.COLUMN_MOVIE_OVERVIEW,
+                FavoritesContract.FavoritesEntry.COLUMN_MOVIE_BACKDROP};
+
+        return new CursorLoader(this,
+                FavoritesContract.FavoritesEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if(!data.moveToFirst()) {
+            emptyView.setVisibility(View.VISIBLE);
+        } else {
+            emptyView.setVisibility(View.GONE);
+        }
+        mMovieCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mMovieCursorAdapter.swapCursor(null);
     }
 
     @Override
